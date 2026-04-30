3b3468dd67f3466bc88fe8ce4b9bd34d1c5f9064
Wawan Gunawan <wansgun1141@gmail.com>
2026-04-30 15:31:32 +0700

    kosongkan query.sql

--- query.sql
+++ query.sql
@@ -4,12 +4,3 @@
 
 -- Cross database example:
 -- SELECT a.*, b.* FROM db1.table_a a JOIN db2.table_b b ON a.id = b.id;
-SELECT
-    * 
-FROM
-    SW_PBB.CPPMOD_PBB_SPPT_FINAL a
-    JOIN SW_PBB.CPPMOD_TAX_KECAMATAN b ON a.CPM_OP_KECAMATAN = b.CPC_TKC_ID
-    JOIN SW_PBB.CPPMOD_TAX_KELURAHAN c ON a.CPM_OP_KELURAHAN = c.CPC_TKL_ID 
-WHERE
-    a.CPM_NOP = a.CPM_WP_NO_KTP
-    limit 100;
